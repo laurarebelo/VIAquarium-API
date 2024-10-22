8245273aebcaa172ddd1b8d7fb350e6ab60d94a0
BiancaBadeu <92147004+BiancaBadeu@users.noreply.github.com>
2024-10-22 18:39:51 +0200

    needs request update
    made a general request to handle petting as well

--- Models/NeedsRequest.cs
+++ Models/NeedsRequest.cs
@@ -3,9 +3,13 @@ using System.ComponentModel.DataAnnotations;
 namespace VIAquarium_API.Models;
 
 [Serializable]
-public class FeedRequest
+public class NeedsRequest
 {
     [Required]
     [Range(1,100)]
     public int hungerPoints { get; set; }
+
+    [Required]
+    [Range(1,100)]
+    public int pettingPoints { get; set; }
 }
