10c6f7b8cc1523ce3626e24c468591328f596e39
BiancaBadeu <92147004+BiancaBadeu@users.noreply.github.com>
2024-11-05 17:13:29 +0100

    added fish death functionality, needs testing

--- Data/AquariumContext.cs
+++ Data/AquariumContext.cs
@@ -5,6 +5,7 @@ public class AquariumContext : DbContext
     public AquariumContext(DbContextOptions<AquariumContext> options) : base(options) { }
 
     public DbSet<Fish> Fish { get; set; }
+    public DbSet<DeadFish> DeadFish { get; set; }  // New DbSet for DeadFish
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -14,5 +15,40 @@ public class AquariumContext : DbContext
         modelBuilder.Entity<Fish>()
             .Property(f => f.Id)
             .ValueGeneratedOnAdd();
+
+        // Configuration for DeadFish
+        modelBuilder.Entity<DeadFish>()
+            .HasKey(df => df.Id);
+
+        modelBuilder.Entity<DeadFish>()
+            .Property(df => df.Id)
+            .ValueGeneratedOnAdd();
+
+        modelBuilder.Entity<DeadFish>()
+            .Property(df => df.Name)
+            .IsRequired()
+            .HasMaxLength(255);
+
+        modelBuilder.Entity<DeadFish>()
+            .Property(df => df.DateOfBirth)
+            .IsRequired();
+
+        modelBuilder.Entity<DeadFish>()
+            .Property(df => df.DateOfDeath)
+            .IsRequired();
+
+        modelBuilder.Entity<DeadFish>()
+            .Property(df => df.CauseOfDeath)
+            .IsRequired()
+            .HasMaxLength(255);
+
+        modelBuilder.Entity<DeadFish>()
+            .Property(df => df.DaysLived)
+            .IsRequired();
+
+        modelBuilder.Entity<DeadFish>()
+            .Property(df => df.RespectCount)
+            .IsRequired()
+            .HasDefaultValue(0);
     }
 }
--- Models/Fish.cs
+++ Models/Fish.cs
@@ -6,11 +6,14 @@ public class Fish
     public DateTime LastUpdatedHunger { get; set; }
     public int SocialLevel { get; set; }
     public DateTime LastUpdatedSocial { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public DateTime? DeathStartTime { get; set; } // Time when hunger reached 0
+    public DateTime? SocialDeathStartTime { get; set; } // Time when social reached 0
 
     public Fish(string name)
     {
-        Id = 0;
         Name = name;
+        DateOfBirth = DateTime.UtcNow;
         ResetNeeds();
     }
 
@@ -18,42 +21,57 @@ public class Fish
     {
         Id = id;
         Name = name;
+        DateOfBirth = DateTime.UtcNow;
         ResetNeeds();
     }
 
     private void ResetNeeds()
     {
         HungerLevel = 100;
-        LastUpdatedHunger = DateTime.Now;
+        LastUpdatedHunger = DateTime.UtcNow;
         SocialLevel = 100;
-        LastUpdatedSocial = DateTime.Now;
+        LastUpdatedSocial = DateTime.UtcNow;
+        DeathStartTime = null;
+        SocialDeathStartTime = null;
     }
 
     public void Feed(int amount)
     {
         HungerLevel += amount;
         if (HungerLevel > 100) HungerLevel = 100;
-        LastUpdatedHunger = DateTime.Now;
+        LastUpdatedHunger = DateTime.UtcNow;
+        DeathStartTime = null;
     }
 
     public void Pet(int amount)
     {
         SocialLevel += amount;
         if (SocialLevel > 100) SocialLevel = 100;
-        LastUpdatedSocial = DateTime.Now;
+        LastUpdatedSocial = DateTime.UtcNow;
+        SocialDeathStartTime = null;
     }
 
     public void GetLonely(int amount)
     {
         SocialLevel -= amount;
         if (SocialLevel < 0) SocialLevel = 0;
-        LastUpdatedSocial = DateTime.Now;
+        LastUpdatedSocial = DateTime.UtcNow;
+        
+        if (SocialLevel == 0 && SocialDeathStartTime == null)
+        {
+            SocialDeathStartTime = DateTime.UtcNow;
+        }
     }
 
     public void GetHungry(int amount)
     {
         HungerLevel -= amount;
         if (HungerLevel < 0) HungerLevel = 0;
-        LastUpdatedHunger = DateTime.Now;
+        LastUpdatedHunger = DateTime.UtcNow;
+
+        if (HungerLevel == 0 && DeathStartTime == null)
+        {
+            DeathStartTime = DateTime.UtcNow;
+        }
     }
 }
